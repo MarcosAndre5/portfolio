d78b366bd6e429d2197b5b4747b95de7280541a0
Marcos André Azevedo de Assis <marcos.andre@funpec.br>
2026-05-12 16:28:45 -0300

    Adicionando imagens.

---
+++ funcoes.js
@@ -0,0 +1,3 @@
+//alert("Página ainda em desenvolvimento.\nEstou escrevendo e melhorando quando posso.\nSe quiser visualizar mesmo assim, clique em 'OK'.")
+
+document.getElementById('anoAtual').textContent = new Date().getFullYear();
