db94704fed421c1e39fd04e334c11c00d2b66773
Marcos André Azevedo de Assis <marcos.andre@funpec.br>
2026-07-08 14:21:55 -0300

    Adicionando alguns classes bootstrap 3.

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,1 @@
-//alert("Página ainda em desenvolvimento.\nEstou escrevendo e melhorando quando posso.\nSe quiser visualizar mesmo assim, clique em 'OK'.")
-
 document.getElementById('anoAtual').textContent = new Date().getFullYear();
